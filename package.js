dde0b49f764f1fe8534897c6d9b86a454c4d4cf3
deeeed <abreton@siteed.net>
2016-01-19 21:19:48 +0800

    Initial commit, not much to say

---
+++ package.js
@@ -0,0 +1,20 @@
+Package.describe({
+    name: 'deeeed:autoform-custom',
+    summary: 'Simple custom fields/forms for autoform',
+    version: '0.1.0',
+    git: 'https://github.com/deeeed/meteor-autoform-custom.git'
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.0.1');
+
+    api.use([
+        'templating',
+        'aldeed:autoform@4.0.0 || 5.0.0'
+    ], 'client');
+
+    api.addFiles([
+        'afFormArrayWrapper/afFormArrayWrapper.html',
+        'afFormArrayWrapper/afFormArrayWrapper.js'
+    ], 'client');
+});
